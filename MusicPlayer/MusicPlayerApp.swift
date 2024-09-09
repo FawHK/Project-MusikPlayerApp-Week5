290f362d5b08c0e639f8b2ac844aa2a31d4540a2
FawHK <fawwaz.hk@gmail.com>
2024-09-09 13:00:02 +0200

    Project created

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,18 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Fawwaz Hajj Khalid on 09.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
